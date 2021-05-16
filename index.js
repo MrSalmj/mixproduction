160100ff1f76c71e849382a37bfb6b978fd7fcff
sal90oo <fifa160016@gmail.com>
2021-05-16 18:17:06 +0300

    init express server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('', async (req, res) => {
     res.send("hello world")
 })
 
-app.listen('443', async (req, res) => {
+app.listen(process.env.PORT || 3000, async (req, res) => {
     console.log('server started')
 })
 console.log("hello heroku")
